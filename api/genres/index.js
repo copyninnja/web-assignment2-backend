38e443e3677e72635b31dd38ca7ab78c4871c9c8
XiaoFeng Gong <20091570@wit.ie>
2021-01-01 18:22:40 +0000

    fix genres

--- api/genres/index.js
+++ api/genres/index.js
@@ -2,14 +2,12 @@ import express from 'express';
 import {
   getGenres
 } from '../tmdb-api';
-import jwt from 'jsonwebtoken';
 
 
 
 const router = express.Router();
 
 router.get('/', (req, res,next) => {
-  const userName = jwt.verify(process.env.TOKEN, process.env.SECRET);
 
     getGenres().then(movies => res.status(200).send(movies))
   .catch((error) => next(error));
